fc09a74a1e49bf0df8bee0fb51d4b40829634ccd
ARNAB HALDER <harnab308@gmail.com>
2025-12-25 22:24:22 +0530

    chat groq add

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
 from langchain_core.globals import set_debug
+from langchain_groq import ChatGroq
+
 
 set_debug(True)
 
 st.title('Ask AI Anything')
 prompt = st.text_input("Enter a question: ")
 
-llm = ChatOllama(model="llama3.2-vision:11b")
+# llm = ChatOllama(model="llama3.2-vision:11b")
+llm = ChatGroq(model="llama3.2-vision:11b", api_key="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")
 
 if prompt:
     res = llm.invoke(prompt)
