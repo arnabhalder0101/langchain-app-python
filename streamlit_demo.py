adb9098237cb74eda297aca08907ee6932216d1f
ARNAB HALDER <harnab308@gmail.com>
2025-12-25 22:06:30 +0530

    requirements.txt added

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 from langchain_ollama import ChatOllama
+from langchain_core.globals import set_debug
+
+set_debug(True)
 
 st.title('Ask AI Anything')
 prompt = st.text_input("Enter a question: ")
